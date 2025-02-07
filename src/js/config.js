b6cb4c7efa7b3fd694353b52043302ec88ae5d65
mainitjerson <117341243+mainitjerson@users.noreply.github.com>
2025-02-07 20:25:14 +0800

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7f67717d-430f-4929-b051-405632497d78';
+export const MODAL_CLOSE_SEC = 2.5;
